51bf4b5315af0238cb39fcb221047a897e3eda8d
Your NameDenis <tdv14011984@gmail.com>
2022-04-27 20:45:20 +0300

    add LambdaStepTestAndListener

--- src/test/java/denis/timushev/LambdaStepTestAndListener.java
+++ src/test/java/denis/timushev/LambdaStepTestAndListener.java
@@ -6,19 +6,16 @@ import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 import static io.qameta.allure.Allure.step;
-import static org.openqa.selenium.By.linkText;
-import static org.openqa.selenium.By.partialLinkText;
 
 @Owner("Helsinki")
 @Severity(SeverityLevel.BLOCKER)
 @Feature("Задачи")
 @Story("Просмотр задач")
 @DisplayName("Тест Issue")
-public class LambdaStepTest {
+public class LambdaStepTestAndListener {
 
     private static final String REPOSITORY = "City-of-Helsinki/hel-icons";
 
